83ac218a9d921f2af8f02678001815dd9613916c
joaopedroaquino <aqerhack@gmail.com>
2023-02-09 17:04:20 -0300

    Create each invalid/valid CPF test

--- 001/test/validator.test.ts
+++ 001/test/validator.test.ts
@@ -1,13 +1,28 @@
 import { validate } from "../src/validator";
 
-test("Deve testar um CPF valido", function(){
-   const isValid = validate("407.302.170-27");
-    expect(isValid).toBeTruthy();
+test.each([
+    "407.302.170-27",
+    "684.053.160-00",
+    "746.971.314-01"])("Deve testar um CPF valido", function (cpf) {
+        const isValid = validate(cpf);
+        expect(isValid).toBeTruthy();
+
+    });
+
+test.each([
+    "302.170-27",
+    "222.053.160-00",
+    "55.971.314-01",
+    "406.302.170-27"
+
+])("Deve testar um CPF invalido", function (cpf) {
+    const isValid = validate(cpf);
+    expect(isValid).toBeFalsy();
 
 });
 
-test("Deve testar um CPF invalido", function(){
-    const isValid = validate("406.302.170-27");
-     expect(isValid).toBeFalsy();
- 
- });
+test("Deve testar um CPF invalido com todos os digitos iguais", function () {
+    const isValid = validate("111.111.111-11");
+    expect(isValid).toBeFalsy();
+
+});
