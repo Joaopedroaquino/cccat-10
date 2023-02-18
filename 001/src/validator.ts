3f2836595e7799599ed0974c03e4757486f33fae
joaopedroaquino <aqerhack@gmail.com>
2023-02-18 20:01:59 -0300

    Create calculate digit

--- 001/src/validator.ts
+++ 001/src/validator.ts
@@ -1,28 +1,21 @@
 // @ts-nocheck
+
+function calculateDigit(cpf, factor) {
+    let total = 0
+    for (const digit of cpf) {
+        if (factor > 1) total += parseInt(digit) * factor--;
+    }
+    const rest = total % 11
+    return (rest < 2) ? 0 : 11 - rest
+}
+
 export function validate(cpf) {
     if (!cpf) return false;
     cpf = cpf.replace(/\D/g, "")
     if (cpf.length !== 11) return false;
     if (cpf.split("").every(c => c === cpf[0])) return false;
-    let d1 = 0;
-    let d2 = 0;
-    let rest = 0;
-    let digito = 0;
-    for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
-        digito = parseInt(cpf.substring(nCount - 1, nCount));
-        d1 = d1 + (11 - nCount) * digito;
-        d2 = d2 + (12 - nCount) * digito;
-    };
-    let dg1 = 0;
-    let dg2 = 0;
-    rest = (d1 % 11);
-    dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-    d2 += 2 * dg1;
-    rest = (d2 % 11);
-    if (rest < 2)
-        dg2 = 0;
-    else
-        dg2 = 11 - rest;
+    const dg1 = calculateDigit(cpf, 10);
+    const dg2 = calculateDigit(cpf, 11);
     let nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
     const nDigResult = "" + dg1 + "" + dg2;
     return nDigVerific == nDigResult;
