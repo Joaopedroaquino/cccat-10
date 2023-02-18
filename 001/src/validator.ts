c08c06af1ba1825553a0cd7c6a98b105a5b40f2d
joaopedroaquino <aqerhack@gmail.com>
2023-02-18 14:20:02 -0300

    Rename CPF

--- 001/src/validator.ts
+++ 001/src/validator.ts
@@ -1,17 +1,17 @@
 // @ts-nocheck
-export function validate (str) {
+export function validate (cpf) {
 
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 && str.length <= 14){
+	if (cpf !== null) {
+        if (cpf !== undefined) {
+            if (cpf.length >= 11 && cpf.length <= 14){
 
-                str=str
+                cpf=cpf
                     .replace('.','')
                     .replace('.','')
                     .replace('-','')
                     .replace(" ","");  
     
-                if (!str.split("").every(c => c === str[0])) {
+                if (!cpf.split("").every(c => c === cpf[0])) {
                     try{  
                         let     d1, d2;  
                         let     dg1, dg2, rest;  
@@ -20,12 +20,12 @@ export function validate (str) {
                         d1 = d2 = 0;  
                         dg1 = dg2 = rest = 0;  
                             
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
+                        for (let nCount = 1; nCount < cpf.length -1; nCount++) {  
+                            // if (isNaN(parseInt(cpf.subcpfing(nCount -1, nCount)))) {
                             // 	return false;
                             // } else {
     
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
+                                digito = parseInt(cpf.substring(nCount -1, nCount));  							
                                 d1 = d1 + ( 11 - nCount ) * digito;  
                 
                                 d2 = d2 + ( 12 - nCount ) * digito;  
@@ -42,7 +42,7 @@ export function validate (str) {
                         else  
                             dg2 = 11 - rest;  
                 
-                            let nDigVerific = str.substring(str.length-2, str.length);  
+                            let nDigVerific = cpf.substring(cpf.length-2, cpf.length);  
                         nDigResult = "" + dg1 + "" + dg2;  
                         return nDigVerific == nDigResult;
                     }catch (e){  
