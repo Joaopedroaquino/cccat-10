8c3c5b1e955f800fe0f5df9713bd9e09a46c1d30
joaopedroaquino <aqerhack@gmail.com>
2023-02-19 12:44:55 -0300

    :whale: Container database in docker-compose :whale:

--- 001/create.sql
+++ 001/create.sql
@@ -7,4 +7,4 @@ create table cccat10.product(
 
 insert into cccat10.product (id_product, description, price) values (1, 'A', 1000);
 insert into cccat10.product (id_product, description, price) values (2, 'B', 5000);
-insert into cccat10.product (id_product, description, price) values (3 'C', 30);
+insert into cccat10.product (id_product, description, price) values (3, 'C', 30);
