4d8045f667a5120db1ab00c6a0215659085836f6
joaopedroaquino <aqerhack@gmail.com>
2023-02-18 21:10:06 -0300

    create database

--- 001/test/main.test.ts
+++ 001/test/main.test.ts
@@ -11,7 +11,7 @@ test("Nao deve aceitar um pedido com cpf invalido", async function () {
 
 });
 
-test("Deve criar um pedido vazio com cpf valido", async function () {
+test("Deve criar um pedido vazio", async function () {
     const input = {
         cpf: "407.302.170-27"
 
@@ -20,5 +20,24 @@ test("Deve criar um pedido vazio com cpf valido", async function () {
     const response = await axios.post("http://localhost:3000/checkout", input);
     const output = response.data;
     expect(output.total).toBe(0);
+
+});
+
+test("Deve criar um pedido com 3 produtos", async function () {
+    const input = {
+        cpf: "407.302.170-27",
+        items: [
+            { idProduct: 1, quantity: 1 },
+            { idProduct: 2, quantity: 1 },
+            { idProduct: 2, quantity: 3 },
+
+
+        ]
+
+
+    }
+    const response = await axios.post("http://localhost:3000/checkout", input);
+    const output = response.data;
+    expect(output.total).toBe(6090);
 
 });
