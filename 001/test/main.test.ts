114d78e74fb43138d3436ea5fc8aa7bb3fbee268
joaopedroaquino <aqerhack@gmail.com>
2023-02-20 10:13:53 -0300

    Create coupon test

--- 001/test/main.test.ts
+++ 001/test/main.test.ts
@@ -41,3 +41,23 @@ test("Deve criar um pedido com 3 produtos", async function () {
     expect(output.total).toBe(6090);
 
 });
+
+test("Deve criar um pedido com 3 produtos com cupom de desconto", async function () {
+    const input = {
+        cpf: "407.302.170-27",
+        items: [
+            { idProduct: 1, quantity: 1 },
+            { idProduct: 2, quantity: 1 },
+            { idProduct: 2, quantity: 3 },
+
+
+        ], 
+        coupon: "VALE20"
+
+
+    }
+    const response = await axios.post("http://localhost:3000/checkout", input);
+    const output = response.data;
+    expect(output.total).toBe(4872);
+
+});
