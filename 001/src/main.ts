1e673b89798ffd1c6eb7f465786466a62e89dfe7
joaopedroaquino <aqerhack@gmail.com>
2023-02-19 22:04:13 -0300

    Update DB

--- 001/src/main.ts
+++ 001/src/main.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from "express";
 import { validate } from "./validator";
+import pgp from "pg-promise"
 const app = express();
 app.use(express.json());
 
-app.post("/checkout", function (req: Request, res: Response) {
+app.post("/checkout", async function (req: Request, res: Response) {
     const output: Output = {
         total: 0
     };
-    const isValid = validate(req.body.cpf)
+    const connection = pgp()("postgres://postgres:123456@localhost:5437/app");
+    if(req.body.items){
+        for (const item of req.body.items){
+            const [productData] = await connection.query("select * from cccat10.product where id_product = $1", item.idProduct);
+            output.total += parseFloat(productData.price) * item.quantity;
+        }
+
+    }
+   
+    const isValid = validate(req.body.cpf);
+    await connection.$pool.end();
     if (!isValid) output.message = "Invalid cpf";
     res.json(output);
 })
