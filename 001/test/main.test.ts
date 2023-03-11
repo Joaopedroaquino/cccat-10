16e57417ad1801f03241614545b7c138d5e91612
joaopedroaquino <aqerhack@gmail.com>
2023-03-11 15:56:21 -0300

    create invalid coupon test

--- 001/test/main.test.ts
+++ 001/test/main.test.ts
@@ -46,3 +46,17 @@ test("Deve criar um pedido com 3 produtos com cupom de desconto", async function
 	const output = response.data;
 	expect(output.total).toBe(4872);
 });
+test("Deve criar um pedido com 3 produtos com cupom de desconto expirado", async function () {
+	const input = {
+		cpf: "407.302.170-27",
+		items: [
+			{ idProduct: 1, quantity: 1 },
+			{ idProduct: 2, quantity: 1 },
+			{ idProduct: 3, quantity: 3 }
+		],
+		coupon: "VALE10"
+	};
+	const response = await axios.post("http://localhost:3000/checkout", input);
+	const output = response.data;
+	expect(output.total).toBe(6090);
+});
